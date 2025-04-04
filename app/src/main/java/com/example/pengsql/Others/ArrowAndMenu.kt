1cd83042b8b23708e3256fb8a047e44adf406ed8
Kim Taeyeong <xodud7737@gmail.com>
2025-04-04 19:10:45 +0900

    _

---
+++ app/src/main/java/com/example/pengsql/Others/ArrowAndMenu.kt
@@ -0,0 +1,40 @@
+package com.example.pengsql.Others
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.pengsql.R
+
+@Composable
+fun ArrowAndMenu(){
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(
+                top = 10.dp,
+                start = 40.dp,
+                end = 40.dp,
+                bottom = 5.dp
+            ),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Icon(
+            painter = painterResource(R.drawable.arrow_back),
+            tint = Color.Black,
+            contentDescription = "",
+        )
+        Icon(
+            painter = painterResource(R.drawable.menu),
+            tint = Color.Black,
+            contentDescription = "",
+        )
+    }
+}
---
+++ app/src/main/java/com/example/pengsql/ui/theme/Color.kt
@@ -0,0 +1,20 @@
+package com.example.pengsql.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+
+// 앱 테마 색상 등록
+val BackGroundColor = Color(216, 224, 227)
+val ButtonColor = Color(62, 108, 108)
+val TitleColor = Color(62, 108, 108)
+val TextColor = Color(18,18,18)
+val TableBackGroundColor = Color(252,252,252)
+val TableLineColor = Color(208,208,208)
