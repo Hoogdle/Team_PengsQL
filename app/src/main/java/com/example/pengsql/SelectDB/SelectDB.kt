f556337ba72a37ca507429e551689a6c35a33104
Kim Taeyeong <xodud7737@gmail.com>
2025-04-05 18:23:51 +0900

    _

--- app/src/main/java/com/example/pengsql/SelectDB/SelectDB.kt
+++ app/src/main/java/com/example/pengsql/SelectDB/SelectDB.kt
@@ -3,6 +3,7 @@ package com.example.pengsql.SelectDB
 import android.text.BoringLayout
 import android.util.Log
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
@@ -17,16 +18,20 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -38,6 +43,7 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.pengsql.Others.ArrowAndMenu
@@ -249,6 +255,17 @@ fun SelectDBParent(
     parentName: String,
     childList: List<String>
 ){
+    // 자식 DropDown condition
+    val isDropDownExpanded = remember {
+        val tmp = mutableStateListOf<Boolean>()
+        for(i in 0..childList.size-1){
+            tmp.add(false)
+        }
+        tmp
+    }
+    // 부모 DropDown condition
+    val isParentDropDownExpanded = remember{ mutableStateOf(false) }
+    val itemPosition = remember {mutableStateOf(0)}
     val isOpened = remember { mutableStateOf(false) }
     if (tableIsOpened.value && isOpened.value) {
         Column {
@@ -279,16 +296,55 @@ fun SelectDBParent(
                 )
             }
             childList.forEachIndexed { index, child ->
-                Row {
-                    Spacer(Modifier.width(60.dp))
-                    Text(
-                        text = child,
-                        style = TextStyle(
-                            color = TextColor,
-                            fontFamily = FontFamily(Font(R.font.roboto_thin)),
-                            fontSize = 14.sp
+                Column {
+                    Spacer(Modifier.height(5.dp))
+                    Row {
+                        Spacer(Modifier.width(70.dp))
+                        Text(
+                            modifier = Modifier
+                                .clickable {
+                                    isDropDownExpanded[index] = true
+                                },
+                            text = child,
+                            style = TextStyle(
+                                color = TextColor,
+                                fontFamily = FontFamily(Font(R.font.roboto_thin)),
+                                fontSize = 14.sp
+                            )
                         )
-                    )
+                        DropdownMenu(
+                            modifier = Modifier
+                                .wrapContentSize()
+                                .background(TableBackGroundColor)
+                                .border(
+                                    width = 1.dp,
+                                    color = TextColor,
+                                    shape = RectangleShape
+                                ),
+                            expanded = isDropDownExpanded[index],
+                            onDismissRequest = {
+                                isDropDownExpanded[index] = false
+                            }
+                        ) {
+                            DropdownMenuItem(
+                                text = {
+                                    Text(
+                                        text = "테이블 보기",
+                                        style = TextStyle(
+                                            color = TextColor,
+                                            fontFamily = FontFamily(Font(R.font.roboto_regular)),
+                                            fontSize = 14.sp
+                                        ),
+                                        overflow = TextOverflow.Ellipsis,
+                                        maxLines = 1
+                                    )
+                                },
+                                onClick = {
+                                    isDropDownExpanded[index] = false
+                                }
+                            )
+                        }
+                    }
                 }
             }
         }
@@ -312,6 +368,10 @@ fun SelectDBParent(
                 )
                 Spacer(Modifier.width(7.dp))
                 Text(
+                    modifier = Modifier
+                        .clickable {
+                            isParentDropDownExpanded.value = true
+                        },
                     text = parentName + " (${childList.size})",
                     style = TextStyle(
                         color = TextColor,
@@ -319,6 +379,57 @@ fun SelectDBParent(
                         fontSize = 14.sp
                     )
                 )
+                DropdownMenu(
+                    modifier = Modifier
+                        .wrapContentSize()
+                        .background(TableBackGroundColor)
+                        .border(
+                            width = 1.dp,
+                            color = TextColor,
+                            shape = RectangleShape
+                        ),
+                    expanded = isParentDropDownExpanded.value,
+                    onDismissRequest = {
+                        isParentDropDownExpanded.value = false
+                    }
+                ) {
+                    DropdownMenuItem(
+                        text = {
+                            Text(
+                                text = "테이블 수정",
+                                style = TextStyle(
+                                    color = TextColor,
+                                    fontFamily = FontFamily(Font(R.font.roboto_regular)),
+                                    fontSize = 14.sp
+                                ),
+                                overflow = TextOverflow.Ellipsis,
+                                maxLines = 1
+                            )
+                        },
+                        onClick = {
+                            isParentDropDownExpanded.value = false
+                            itemPosition.value = 0
+                        }
+                    )
+                    DropdownMenuItem(
+                        text = {
+                            Text(
+                                text = "테이블 삭제",
+                                style = TextStyle(
+                                    color = TextColor,
+                                    fontFamily = FontFamily(Font(R.font.roboto_regular)),
+                                    fontSize = 14.sp
+                                ),
+                                overflow = TextOverflow.Ellipsis,
+                                maxLines = 1
+                            )
+                        },
+                        onClick = {
+                            isParentDropDownExpanded.value = false
+                            itemPosition.value = 1
+                        }
+                    )
+                }
             }
         }
     }
